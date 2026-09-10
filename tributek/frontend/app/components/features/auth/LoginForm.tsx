8a85a1769c5304eba77f1c7a4fc0dcde08d04dc4
BrandonLCC <br.casas@duocuc.cl>
2026-09-10 16:43:01 -0300

    Actualizando documentación antes de implementar la BD con prisma y posgreSQL.

--- tributek/frontend/app/components/features/auth/LoginForm.tsx
+++ tributek/frontend/app/components/features/auth/LoginForm.tsx
@@ -8,7 +8,7 @@ export default function LoginForm() {
             <img
                 src="/images/tras-TRIBUTEK.svg"
                 alt="Logo TRIBUTEK"
-                className="mx-auto h-32 w-64 object-contain"
+                className="mx-auto h-35 w-100 object-contain"
             />
             <div className="flex flex-col items-center justify-center text-center text-[#252f46]">
                 <h3 className="text-xl font-bold">BIENVENIDO/A</h3>
