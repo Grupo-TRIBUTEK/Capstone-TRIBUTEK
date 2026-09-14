fa273685fa207468721577b418ca7d4ca263b35d
Nazabkn <naz.gonzalez@duocuc.cl>
2026-09-14 01:28:35 -0300

    Protección de consultas y creación d clientes para admins

--- tributek/backend/src/clientes/clientes.controller.ts
+++ tributek/backend/src/clientes/clientes.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ClientesService } from './clientes.service.js';
 import { CreateClienteDto } from './dto/create-cliente.dto.js';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard.js';
+import { RolesGuard } from '../auth/roles.guard.js';
+import { Roles } from '../auth/decorators/roles.decorator.js';
 
 // exponemos el endpoint que recibirá los datos del formulario.
 
@@ -10,12 +12,15 @@ export class ClientesController {
   constructor(private readonly clientesService: ClientesService) {}
 
   @Post()
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(1)
   crearCliente(@Body() datos: CreateClienteDto) {
     return this.clientesService.crearCliente(datos);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(1)
   obtenerClientes() {
     return this.clientesService.obtenerClientes();
   }
